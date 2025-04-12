868e6bf3555ab03b98797ffda06aa371d6915c0c
ProBaconHub <ProBaconHub@gmail.com>
2025-04-12 17:49:43 +0800

    Update gc_module.lua

--- Universal Functions/gc_module.lua
+++ Universal Functions/gc_module.lua
@@ -1,9 +1,22 @@
-return function(input)
-    input = input or nil
+return function(input, andlist, orlist)
+    input = input or false
     local output = {}
     for _,v in pairs(getgc(input)) do
         if input and type(v) == "table" then
-            table.insert(output, v)
+            local isLegit = true
+            for _,andItem in pairs(andlist) do
+                if rawget(v, andItem) == nil then
+                    isLegit = false
+                end
+            end
+            if isLegit then
+                for _,orItem in pairs(orlist) do
+                    if rawget(v, orItem) ~= nil then
+                        table.insert(output, v)
+                        break
+                    end
+                end
+            end
         elseif not input then
             table.insert(output, v)
         end
